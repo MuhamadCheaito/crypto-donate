013294d2ae579e81ef2a3bfeb9df4a6c887eae96
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2021-09-06 21:24:05 +0300

    install web3 & configure homepage

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const web3 = require('web3');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req,res) => {
-    res.render("\home");
+    res.render('\home');
 });
 
-app.listen(5000,()=>{
-    console.log("listening to port 5000");
+app.listen(4000,()=>{
+    console.log("listening to port 4000");
 });
-module.exports = app;
